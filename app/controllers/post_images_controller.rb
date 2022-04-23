80fb59c10f7eef11f022138022bc1fb359297fef
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-04-23 02:15:46 +0000

    [Finish]4-13

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -13,6 +13,12 @@ class PostImagesController < ApplicationController
   def index
     @post_images = PostImage.all
   end
+  
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to '/post_images'
+  end
 
   def show
     @post_image = PostImage.find(params[:id])
