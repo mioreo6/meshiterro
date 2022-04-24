346bdafa04c35cfb64dd88f078295b194a03e111
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-04-24 13:20:42 +0000

    [Finish]4-21

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -20,6 +20,16 @@ class PostImagesController < ApplicationController
     redirect_to '/post_images'
   end
 
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
+  end
+  
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
