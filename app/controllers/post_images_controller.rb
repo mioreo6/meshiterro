854866ddf853f527d142c915553b9e35e010dc39
mioreo6 <102738892+mioreo6@users.noreply.github.com>
2022-04-25 15:10:48 +0000

    [add]4-24

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -9,11 +9,11 @@ class PostImagesController < ApplicationController
     @post_image.save
     redirect_to post_images_path
   end
-    
+
   def index
     @post_images = PostImage.page(params[:page])
   end
-  
+
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
@@ -29,15 +29,15 @@ class PostImagesController < ApplicationController
       render :new
     end
   end
-  
+
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
   private
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
+
 end
